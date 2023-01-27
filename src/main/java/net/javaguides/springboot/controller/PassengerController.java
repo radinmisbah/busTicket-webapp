7063d8765c30220a5047cbe61993a28338f8f534
radinmisbah <79858161+radinmisbah@users.noreply.github.com>
2023-01-27 08:08:46 +0800

    add search Form for trip

--- src/main/java/net/javaguides/springboot/controller/PassengerController.java
+++ src/main/java/net/javaguides/springboot/controller/PassengerController.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.javaguides.springboot.model.Booking;
 import net.javaguides.springboot.model.Trip;
 import net.javaguides.springboot.model.User;
 import net.javaguides.springboot.service.TripService;
@@ -28,18 +30,16 @@ public class PassengerController {
 		Trip trip = new Trip();
 		model.addAttribute("trip", trip);
 
-        User user = new User();
-        model.addAttribute("user", user);
 		return "search_trip";
 	}
 
     //controller mapping for user page	
 	@GetMapping("/user/viewTripList")
-	public String searchTicket(Model model, @RequestParam("departure") String departure, @RequestParam("arrival") String arrival, @RequestParam("departureDate") Date departureDate) {
+	public String searchTicket(Model model, @RequestParam("departure") String departure, @RequestParam("arrival") String arrival, @RequestParam("departureDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date departureDate) {
 		List<Trip> buses = tripService.searchBus(departure, arrival);
 		model.addAttribute("buses", buses);
 		//departureDate is used for filtering availability
-		return "search_bus";
+		return "view_trip_list";
 	}
 
     
